3b5d070c0abb426bd751a5d45f5061000d07b655
Misash <misash@outlook.com>
2022-09-14 06:16:28 -0500

    print Tree functional version

--- UCSP/printTree.cpp
+++ UCSP/printTree.cpp
@@ -36,8 +36,8 @@ public:
     int Size();
     void InOrderTrack(CBinTreeNode<T>*  , vector<vector<string>> & , int , int &);
     void PrintNiveles();
-    void PrintRight(string& sb, string padding, string pointer , CBinTreeNode<T> * node);
-    void PrintLeft(string& sb, string padding, string pointer , CBinTreeNode<T> * node);
+    void RotateVec(vector<vector<string>> a , vector<vector<string>> &b, string);
+
 
 
 private:
@@ -96,20 +96,6 @@ int CBinTree<T>::MaxDepth(CBinTreeNode<T>* n)
 
 
 
-/* ---  IMPLEMENTACIONES  ---*/
-
-//template <class T>
-//void CBinTree<T>::InOrderTrack(CBinTreeNode<T>* data , vector<vector<string>> &v ,  int i, int &j)
-//{
-//    if ( !data ) return;
-//    InOrderTrack(data->nodes[0],v,i+1,j);
-//    v[i][j++] = std::to_string(data->value);
-//    InOrderTrack(data->nodes[1],v,i+1,j);
-//}
-
-
-
-
 template <class T>
 void CBinTree<T>::InOrderTrack(CBinTreeNode<T>* data , vector<vector<string>> &v ,  int i, int &j)
 {
@@ -119,6 +105,14 @@ void CBinTree<T>::InOrderTrack(CBinTreeNode<T>* data , vector<vector<string>> &v
     InOrderTrack(data->nodes[1],v,i+1,j);
 }
 
+void printVec(vector<vector<string>> vec){
+    cout<<"\n\n";
+    for (int i = 0; i < vec.size(); ++i) {
+        for (int j = 0; j < vec[0].size(); ++j)
+            cout<<vec[i][j]<<" ";
+        cout<<"\n";
+    }
+}
 
 template <class T>
 void CBinTree<T>::PrintNiveles() {
@@ -126,120 +120,118 @@ void CBinTree<T>::PrintNiveles() {
     string delimiter = " ";
 
     int height = 2*MaxDepth(root) + 1 , j = 0;
-    vector<vector<string>> vec(height, vector<string> (size, delimiter));
+    vector<vector<string>> down(height, vector<string> (size, delimiter));
 
-    InOrderTrack(root,vec,0,j);
+    InOrderTrack(root, down, 0, j);
 
 
     cout<<"\n\n";
     for (int i = 0; i < height; ++i) {
         for (j = 0; j < size; ++j)
-            if(vec[i][j] != delimiter && vec[i][j] != "+"  ){
+            if(down[i][j] != delimiter && down[i][j] != "+"  ){
                 CBinTreeNode<T>** p;
-                if ( Find(stoi(vec[i][j]),p) ){
+                if ( Find(stoi(down[i][j]), p) ){
                     //check nodes
                     for (int x = 0; x <=1 ; ++x) {
                         if((*p)->nodes[x]){
                             int k = j;
-                            while(vec[i+2][k] == delimiter  || vec[i+2][k] == "+" ){
-                                vec[i+1][k]="+";
+                            while(down[i + 2][k] == delimiter || down[i + 2][k] == "+" ){
+                                down[i + 1][k]="+";
                                 k = (x)? k+1 : k-1;
                             }
-                            vec[i+1][k]="+";
+                            down[i + 1][k]="+";
                         }
                     }
 
                 }
             }
-
-
-
     }
 
+    vector<vector<string>> left;
+    vector<vector<string>> up;
+    vector<vector<string>> right;
 
-    cout<<"\n\n";
-    for (int i = 0; i < height; ++i) {
-        for (j = 0; j < size; ++j)
-            cout<<vec[i][j]<<"\t";
-        cout<<"\n";
-    }
+    printVec(down);
 
-//    vector<vector<string>> vec2(size, vector<string> (height, "|"));
-//
-//    cout<<"\n\n";
-//    for (int i = 0; i < height; ++i) {
-//        for (j = 0; j < size; ++j)
-//            vec2[j][height - 1 -i] = vec[i][j];
-//    }
-//
-//
-//
-//    for (int i = 0; i < vec2.size(); ++i) {
-//        for (j = 0; j < vec2[0].size(); ++j)
-//            cout<<vec2[i][j]<<" ";
-//        cout<<"\n";
-//    }
+    RotateVec(down, left,delimiter);
+    printVec(left);
+
+    RotateVec(left, up,delimiter);
+    printVec(up);
+
+    RotateVec(up, right,delimiter);
+    printVec(right);
+
 
 }
 
 template <class T>
-void CBinTree<T>::PrintRight(string& sb, string padding, string pointer , CBinTreeNode<T> * node){
-    if(!node) return;
+void CBinTree<T>::RotateVec(vector<vector<string>> a , vector<vector<string>>& b , string delimiter){
 
-    sb += padding;
-    sb += pointer;
-    sb += to_string(node->value);
-    sb += "\n";
+    b.resize(a[0].size(), vector<string> (a.size(), delimiter));
 
-    string paddingForBoth = padding;
-    if( node != root)
-        paddingForBoth += (pointer == "└── ") ? "   " : "│  " ;
+    int row = b.size() , col = b[0].size();
 
-
-    for (int i = 0; i <= 1; ++i) {
-        if(node->nodes[0] && node->nodes[1])
-            pointer = (node->nodes[i] && i)?  "└── " : "├── ";
-        else if(node->nodes[i])
-            pointer =  "└── ";
-        PrintRight(sb, paddingForBoth, pointer, node->nodes[i]);
+    for (int i = 0; i < a.size(); ++i) {
+        for (int j = 0; j < a[0].size(); ++j)
+            b[j][ a.size() - 1 -i] = a[i][j];
     }
 
 }
 
 
+//template <class T>
+//void CBinTree<T>::PrintRight(string& sb, string padding, string pointer , CBinTreeNode<T> * node){
+//    if(!node) return;
+//
+//    sb += padding;
+//    sb += pointer;
+//    sb += to_string(node->value);
+//    sb += "\n";
+//
+//    string paddingForBoth = padding;
+//    if( node != root)
+//        paddingForBoth += (pointer == "└── ") ? "   " : "│  " ;
+//
+//
+//    for (int i = 0; i <= 1; ++i) {
+//        if(node->nodes[0] && node->nodes[1])
+//            pointer = (node->nodes[i] && i)?  "└── " : "├── ";
+//        else if(node->nodes[i])
+//            pointer =  "└── ";
+//        PrintRight(sb, paddingForBoth, pointer, node->nodes[i]);
+//    }
+//
+//}
+//
+//
+//
+//template <class T>
+//void CBinTree<T>::PrintLeft(string& sb, string padding, string pointer , CBinTreeNode<T> * node){
+//    if(!node) return;
+//
+//    sb += padding;
+//    sb += pointer;
+//    sb += to_string(node->value);
+//    sb += "\n";
+//
+//    string paddingForBoth = padding;
+//    if( node != root)
+//        paddingForBoth += (pointer == "└── ") ? "   " : "│  " ;
+//
+//
+//    for (int i = 0; i <= 1; ++i) {
+//        if(node->nodes[0] && node->nodes[1])
+//            pointer = (node->nodes[i] && i)?  "└── " : "├── ";
+//        else if(node->nodes[i])
+//            pointer =  "└── ";
+//        PrintRight(sb, paddingForBoth, pointer, node->nodes[i]);
+//    }
+//
+//}
 
-template <class T>
-void CBinTree<T>::PrintLeft(string& sb, string padding, string pointer , CBinTreeNode<T> * node){
-    if(!node) return;
-
-    sb += padding;
-    sb += pointer;
-    sb += to_string(node->value);
-    sb += "\n";
-
-    string paddingForBoth = padding;
-    if( node != root)
-        paddingForBoth += (pointer == "└── ") ? "   " : "│  " ;
 
 
-    for (int i = 0; i <= 1; ++i) {
-        if(node->nodes[0] && node->nodes[1])
-            pointer = (node->nodes[i] && i)?  "└── " : "├── ";
-        else if(node->nodes[i])
-            pointer =  "└── ";
-        PrintRight(sb, paddingForBoth, pointer, node->nodes[i]);
-    }
-
-}
-void reverseStr(string& str)
-{
-    int n = str.length();
-
-    // Swap character starting from two
-    // corners
-    for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - i - 1]);
-}
 
 template <class T>
 void CBinTree<T>::Print()
@@ -269,11 +261,9 @@ int main()
     }
 
 
-    cout<<"\nArbol v";
+    cout<<"\noutput";
     t.PrintNiveles();
 
-    cout<<"\nArbol ->";
-    t.Print();
 
 
 }
